a76b0c0dd8738120a5edd660457c1a4f612402cc
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-02-09 20:11:36 +0530

    adding arthematic program

---
+++ arthematic_ops.py
@@ -0,0 +1,14 @@
+a=int(input("enter first num:" ))
+b=int(input("enter second  num:" ))
+print(f"sum of {a} , {b} : ",a+b)
+print(f"diff of {a} , {b} : ",a-b)
+print(f"multi of {a} , {b} : ",a*b)
+print(f"div of {a} , {b} : ",a/b)
+print(f"mod of {a} , {b} : ",a%b)
+
+
+
+
+  
+
+
